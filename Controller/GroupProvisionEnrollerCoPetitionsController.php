2d89ea58a794b65df1dd6e5ce26013d46b6fd3e8
Scott Koranda <skoranda@gmail.com>
2024-06-20 08:08:28 -0500

    first commit

---
+++ Controller/GroupProvisionEnrollerCoPetitionsController.php
@@ -0,0 +1,67 @@
+<?php
+
+// This COmanage Registry enrollment plugin is intended to be used
+// with a self-signup enrollment flow that
+// automatically adds users to a CO Group. It forces the
+// CO group object to be provisioned when the enrollment flow
+// completes.
+//
+// The following enrollment steps are implemented:
+//
+// provision:
+//   - invokes manualProvision() on the CO Group(s)
+
+App::uses('CoPetitionsController', 'Controller');
+App::uses('HtmlHelper', 'View/Helper');
+ 
+class GroupProvisionEnrollerCoPetitionsController extends CoPetitionsController {
+  // Class name, used by Cake
+  public $name = "GroupProvisionEnrollerCoPetitions";
+  public $uses = array(
+    "CoPetition",
+    "GroupProvisionEnroller.GroupProvisionEnroller"
+  );
+
+
+  /**
+   * Plugin functionality following provision step
+   *
+   * @param Integer $id CO Petition ID
+   * @param Array $onFinish URL, in Cake format
+   */
+   
+  protected function execute_plugin_provision($id, $onFinish) {
+    // Pull the petition and its related enrollment flow configuration.
+    $args = array();
+    $args['conditions']['CoPetition.id'] = $id;
+    $args['contain']['CoEnrollmentFlow']['CoEnrollmentAttribute'] = 'CoEnrollmentAttributeDefault';
+
+    $petition = $this->CoPetition->find('first', $args);
+    $this->log("Provision: Petition is " . print_r($petition, true));
+
+    // Examine the enrollment flow configuration to determine which, if any,
+    // CO Groups should be re-provisioned.
+    $groupIds = array();
+
+    if(!empty($petition['CoEnrollmentFlow']['CoEnrollmentAttribute'])) {
+      foreach($petition['CoEnrollmentFlow']['CoEnrollmentAttribute'] as $attribute) {
+        if($attribute['attribute'] == 'g:co_group_member') {
+          if(!empty($attribute['CoEnrollmentAttributeDefault'][0]['value'])) {
+            $groupId = $attribute['CoEnrollmentAttributeDefault'][0]['value'];
+            $groupIds[] = $groupId;
+          }
+        }
+      }
+    }
+
+    $this->log("Provision: will force provisioning of CO Groups with IDs " . print_r($groupIds, true));
+
+    // Invoke a manual re-provision on the CO Group.
+    foreach($groupIds as $groupId) {
+      $this->CoPetition->Co->CoGroup->manualProvision(null, null, $groupId);
+      $this->log("Provision: reprovsioned CO Group with ID $groupId");
+    }
+
+    $this->redirect($onFinish);
+  }
+}
---
+++ Lib/lang.php
@@ -0,0 +1,8 @@
+<?php
+
+$cm_group_provision_enroller_texts['en_US'] = array(
+
+  // Error messages
+
+  // Plugin texts
+);
